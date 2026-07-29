bd3e5f9238fc99a142781498aee208898f092f1f
JustCallMeJade <rotheory13@gmail.com>
2026-07-29 20:51:40 +0800

    Update build-termux.sh

--- build-termux.sh
+++ build-termux.sh
@@ -21,3 +21,70 @@ wget -O Rootfs.tar https://github.com/JustCallMeJade/TermuxFS-RootFS/releases/do
 
 tar -xf Rootfs.tar
 
+export rfs="$WORKDIR/data/data/com.termux/files/usr"
+
+export PKG_CONFIG_PATH="$rfs/lib/pkgconfig:$rfs/share/pkgconfig"
+
+export PKG_CONFIG_LIBDIR="$rfs/lib/pkgconfig:$rfs/share/pkgconfig"
+
+export NDK-BIN="$WORKDIR/android-ndk-r30-beta2/toolchains/llvm/prebuilt/linux-x86_64/bin"
+
+git clone --depth=1 https://gitlab.freedesktop.org/mesa/mesa.git
+
+cd mesa
+
+cat <<EOF > android.txt
+[binaries]
+ar = '$ndk/llvm-ar'
+c = ['$ndk/aarch64-linux-android28-clang', '-fno-exceptions', '-fno-unwind-tables', '-fno-asynchronous-unwind-tables', '--start-no-unused-arguments', '-static-libstdc++', '--end-no-unused-arguments', '-Wno-error']
+cpp = ['$NDK-BKN/aarch64-linux-android28-clang++', '-fno-exceptions', '-fno-unwind-tables', '-fno-asynchronous-unwind-tables', '--start-no-unused-arguments', '-static-libstdc++', '--end-no-unused-arguments', '-Wno-error']
+c_ld = '$NDK-BIN/ld.lld'
+cpp_ld = '$NDK-BIN/ld.lld'
+strip = '$NDK-BIN/llvm-strip'
+pkg-config = ['env', 'PKG_CONFIG_LIBDIR=$rfs/lib/pkg-config:$rfs/share/pkg-config', 'PKG_CONFIG_PATH=$rfs/lib/pkgconfig:$rfs/lib/pkgconfig', 'pkg-config']
+
+[properties]
+sys_root = '$sysroot'
+
+[host_machine]
+system = 'android'
+cpu_family = 'aarch64'
+cpu = 'armv8'
+endian = 'little'
+EOF
+
+cat <<EOF > native.txt
+[binaries]
+c = 'clang'
+cpp = 'clang++'
+ar = 'llvm-ar'
+strip = 'llvm-strip'
+c_ld = 'ld.lld'
+cpp_ld = 'ld.lld'
+pkg-config = 'pkg-config'
+
+[build_machine]
+system = 'linux'
+cpu_family = 'x86_64'
+cpu = 'x86_64'
+endian = 'little'
+EOF
+
+meson setup build-android-aarch64 \
+    --cross-file android.txt \
+    --native-file native.txt \
+    --prefix "$OUTPUT" \
+    -Dbuildtype=debugoptimized \
+    -Dstrip=true \
+    -Dplatforms=android,x11 \
+    -Dvideo-codecs=all \
+    -Dplatform-sdk-version=28 \
+    -Dandroid-stub=true \
+    -Dgallium-drivers= \
+    -Dvulkan-drivers=freedreno \
+    -Dvulkan-beta=true \
+    -Dfreedreno-kmds=kgsl \
+    -Degl=disabled \
+    -Dandroid-strict=false
+
+    ninja -C build-aarch64 -j$(nproc) install
