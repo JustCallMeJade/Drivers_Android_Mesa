23d369e161a34a0a2cf5a884e7132992c8310b27
JustCallMeJade <rotheory13@gmail.com>
2026-08-01 13:31:51 +0800

    Initialize PanVK build script for Android
    This script sets up the build environment for PanVK, installs dependencies, and compiles the Mesa graphics driver for Android. It also packages the resulting binaries into a zip file.

---
+++ Build-PanVK.sh
@@ -0,0 +1,163 @@
+#!/bin/bash
+set -uo pipefail
+
+workdir="$(pwd)/pan_workdir"
+ndk="$workdir/r29/toolchains/llvm/prebuilt/linux-x86_64/bin"
+sysroot="$workdir/r29/toolchains/llvm/prebuilt/linux-x86_64/sysroot"
+mesasrc="https://github.com/leegao/mesa-26.2.git"
+deps="git pkg-config cmake build-essential wget2 patchelf zip"
+VERSION="26.3.0-V3.0"
+
+if [[ -z "${API_VER:-}" ]]; then
+    echo "API_VER is not set. Select an API version:"
+    select ver in 27 28 29 30 31 32 33 34 35 36; do
+        if [[ -n "$ver" ]]; then
+            API_VER="$ver"
+            export API_VER
+            break
+        fi
+        echo "Invalid selection."
+    done
+fi
+
+echo "Only works in debian Arm64!!! press Ctrl + C to exit"
+echo "Installing build dependencies..."
+
+sudo sed -i '/^Types:/{/deb-src/! s/$/ deb-src/;}' /etc/apt/sources.list.d/debian.sources
+
+sudo apt-get update -y > /dev/null 2>&1
+sudo apt-get build-dep mesa -y -qq > /dev/null 2>&1
+sudo apt-get build-dep libarchive -y -qq > /dev/null 2>&1
+sudo apt install -y $deps > /dev/null 2>&1
+sudo apt install git pkg-config cmake patchelf build-essential wget2 zip # fallback when deps installation failed
+
+mkdir -p "$workdir" && cd "$workdir"
+mkdir -p "$workdir/pan"
+
+rm -rf "$workdir/r29"
+rm -rf "$workdir/mesa"
+rm -rf "$workdir/android-ndk-r29-linux-aarch64.tar.gz"
+
+cd "$workdir"
+wget2 -q -nv https://github.com/SnowNF/ndk-aarch64-linux/releases/download/0.0.2/android-ndk-r29-linux-aarch64.tar.gz
+tar -xzf android-ndk-r29-linux-aarch64.tar.gz
+
+git clone "$mesasrc" --depth=1
+cd mesa
+
+git config user.name "PanVK-Builder"
+git config user.email "sdddxd86@gmail.com"
+git switch test-kbase
+
+rm -f VERSION 
+cat <<EOF > VERSION
+$VERSION
+EOF
+
+sed -i 's/anb->handle->/((const native_handle_t \*)anb->handle)->/g' src/vulkan/runtime/vk_android.c || true
+sed -i 's/typedef const native_handle_t\* buffer_handle_t;/typedef void\* buffer_handle_t;/g' include/android_stub/cutils/native_handle.h || true
+sed -i 's/, hnd->handle/, (void \*)hnd->handle/g' src/util/u_gralloc/u_gralloc_fallback.c || true
+sed -i 's/native_buffer->handle->/((const native_handle_t \*)native_buffer->handle)->/g' src/vulkan/runtime/vk_android.c || true
+
+export PATH="$ndk:$PATH"
+export CC=clang
+export CXX=clang++
+export AR=llvm-ar
+export RANLIB=llvm-ranlib
+export STRIP=llvm-strip
+export OBJDUMP=llvm-objdump
+export OBJCOPY=llvm-objcopy
+export LDFLAGS="-fuse-ld=lld"
+
+cat <<EOF > android-aarch64.txt
+[binaries]
+ar = '$ndk/llvm-ar'
+c = ['$ndk/aarch64-linux-android$API_VER-clang', '--sysroot=$sysroot', '-fno-exceptions', '-fno-unwind-tables', '-fno-asynchronous-unwind-tables', '--start-no-unused-arguments', '-static-libstdc++', '--end-no-unused-arguments', '-Wno-error']
+cpp = ['$ndk/aarch64-linux-android$API_VER-clang++', '--sysroot=$sysroot', '-fno-exceptions', '-fno-unwind-tables', '-fno-asynchronous-unwind-tables', '--start-no-unused-arguments', '-static-libstdc++', '--end-no-unused-arguments', '-Wno-error']
+c_ld = '$ndk/ld.lld'
+cpp_ld = '$ndk/ld.lld'
+strip = '$ndk/llvm-strip'
+pkg-config = ['env', 'PKG_CONFIG_LIBDIR=$sysroot/usr/lib/pkg-config', 'PKG_CONFIG_SYSROOT_DIR=$sysroot', '/usr/bin/pkg-config']
+
+[built-in options]
+c_args = ['--sysroot=$sysroot', '-Wno-error']
+cpp_args = ['--sysroot=$sysroot']
+c_link_args = ['--sysroot=$sysroot']
+cpp_link_args = ['--sysroot=$sysroot']
+
+[properties]
+sys_root = '$sysroot'
+
+[host_machine]
+system = 'android'
+cpu_family = 'aarch64'
+cpu = 'armv8'
+endian = 'little'
+EOF
+
+cat <<EOF > native.txt
+[binaries]
+c = 'clang'
+cpp = 'clang++'
+ar = 'llvm-ar'
+strip = 'llvm-strip'
+c_ld = 'ld.lld'
+cpp_ld = 'ld.lld'
+pkg-config = 'pkg-config'
+
+[build_machine]
+system = 'linux'
+cpu_family = 'aarch64'
+cpu = 'armv8'
+endian = 'little'
+EOF
+
+meson setup build-android-aarch64 || exit 1 \
+    --cross-file android-aarch64.txt \
+    --native-file native.txt \
+    --prefix "$workdir/pan" \
+    -Dbuildtype=debugoptimized \
+    -Dstrip=true \
+    -Dplatforms=android \
+    -Dvideo-codecs=all \
+    -Dplatform-sdk-version="$API_VER" \
+    -Dandroid-stub=true \
+    -Dgallium-drivers= \
+    -Dvulkan-drivers=panfrost \
+    -Dvulkan-beta=true \
+    -Degl=disabled \
+    -Dandroid-strict=false \
+    -Dallow-fallback-for=libdrm
+
+ninja -C build-android-aarch64 -j"$(nproc)" install || exit 1
+
+cd "$workdir/pan/lib"
+
+echo "packaging PanVK"
+
+patchelf --set-soname vulkan.mali.so libvulkan_panfrost.so
+mv libvulkan_panfrost.so vulkan.mali.so
+
+cat <<EOF > meta.json
+{
+"schemaVersion": 1,
+"name": "Mesa PanVK v$VERSION",
+"description": "Built from Mesa source + GPU hacks",
+"author": "JustCallMeJade",
+"packageVersion": "1",
+"vendor": "Mesa3D, Leegao",
+"driverVersion": "Vulkan 1.4",
+"minApi": 28,
+"libraryName": "vulkan.mali.so"
+}
+EOF
+
+zip -9 "$workdir/pan/PanVK-v$VERSION.zip" vulkan.mali.so meta.json
+
+if [ "${GITHUB_ACTIONS:-}" = "true" ]; then
+    echo "VERSION=$VERSION" >> "$GITHUB_ENV"
+fi
+
+echo "build complete."
+
+exit 0
